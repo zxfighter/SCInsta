d4296735030715cce03b562f369ead4d945f4b82
SoCuul <63339559+SoCuul@users.noreply.github.com>
2024-05-16 19:50:58 -0700

    feat: improve & log errors for cache deletion

--- src/Manager.m
+++ src/Manager.m
@@ -95,14 +95,41 @@
 
 
 + (void)cleanCache {
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    NSMutableArray<NSError *> *deletionErrors = [NSMutableArray array];
+
     // Temp folder
-    BOOL tempFolderSuccess = [[NSFileManager defaultManager] removeItemAtURL:[NSURL fileURLWithPath:NSTemporaryDirectory()] error:nil];
-    if (!tempFolderSuccess) NSLog(@"[SCInsta] Error deleting temp folder");
+    NSError *tempFolderError;
+    [fileManager removeItemAtURL:[NSURL fileURLWithPath:NSTemporaryDirectory()] error:&tempFolderError];
+
+    if (tempFolderError) [deletionErrors addObject:tempFolderError];
 
     // Analytics folder
+    NSError *analyticsFolderError;
     NSString *analyticsFolder = [[NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES) objectAtIndex:0] stringByAppendingPathComponent:@"Application Support/com.burbn.instagram/analytics"];
-    BOOL analyticsFolderSuccess = [[NSFileManager defaultManager] removeItemAtURL:[[NSURL alloc] initFileURLWithPath:analyticsFolder] error:nil];
-    if (!analyticsFolderSuccess) NSLog(@"[SCInsta] Error deleting analytics folder");
+    [fileManager removeItemAtURL:[[NSURL alloc] initFileURLWithPath:analyticsFolder] error:&analyticsFolderError];
+
+    if (analyticsFolderError) [deletionErrors addObject:analyticsFolderError];
+    
+    // Caches folder
+    NSString *cachesFolder = [[NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES) objectAtIndex:0] stringByAppendingPathComponent:@"Caches"];
+    NSArray *cachesFolderContents = [fileManager contentsOfDirectoryAtURL:[[NSURL alloc] initFileURLWithPath:cachesFolder] includingPropertiesForKeys:nil options:NSDirectoryEnumerationSkipsHiddenFiles error:nil];
+    
+    for (NSURL *fileURL in cachesFolderContents) {
+        NSError *cacheItemDeletionError;
+        [fileManager removeItemAtURL:fileURL error:&cacheItemDeletionError];
+
+        if (cacheItemDeletionError) [deletionErrors addObject:cacheItemDeletionError];
+    }
+
+    // Log errors
+    if (deletionErrors.count > 1) {
+
+        for (NSError *error in deletionErrors) {
+            NSLog(@"[SCInsta] File Deletion Error: %@", error);
+        }
+
+    }
 
 }
 + (BOOL)isEmpty:(NSURL *)url {
